604aad09b07067e0e00d9504a4723c60c6c06a27
EFFII-haker <darkxgold514@gmail.com>
2025-07-26 02:34:39 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "", // Put Session Id Here Inside "SUBZERO-MD;;;Wc02BLII#VCGFRC9NA8zsM8yPR-NOijd_8yUxH3C7mlv2JEC6JzA"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
